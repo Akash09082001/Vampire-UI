4bf17d418ce1c01ac9529e00b197c88607fb972d
Akash09082001 <aakashl@letsupgrade.in>
2024-08-22 16:41:48 +0530

    add: theme toggle added

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import ThemeToggle from "@/components/atoms/ThemeToggle"
+
+const Home = () => {
+	return (
+		<div className="flex h-full w-full items-center justify-center">
+			<ThemeToggle />
+		</div>
+	)
+}
+
+export default Home
